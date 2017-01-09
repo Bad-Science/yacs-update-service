0f245fdab750b0814a2c0cf234b971f8523c8f71
Richie <richiejoeyoung@gmail.com>
2017-01-09 14:32:42 -0500

    add courses update [wip]

--- lib/yacs_update_service.rb
+++ lib/yacs_update_service.rb
@@ -1,16 +1,22 @@
+require 'open-uri'
 require 'rest-client'
+require 'nokogiri'
 
 module YacsUpdateService
   SEMESTER = '201701'
   API_URL = 'yacs/api/v5/'
+  AUTH_HEADER = { Authorization: 'Token token=xyz' }
 
   def full_update
-    
+    courses = get_courses
+    RestClient.put("#{API_URL}/courses/bulk", { courses: courses }, AUTH_HEADER)
+    ids = courses.map { |c| c[:id] }
+    RestClient.put("#{API_URL}/courses/active", { id: ids.join(',') }, AUTH_HEADER)
   end
 
   def quick_update
     sections = get_sections
-    RestClient.put("#{API_URL}/sections/bulk_update")
+    RestClient.put("#{API_URL}/sections/bulk", { sections: sections }, AUTH_HEADER)
   end
 
   private
@@ -25,6 +31,6 @@ module YacsUpdateService
   end
 
   def get_courses
-    
+
   end
 end
